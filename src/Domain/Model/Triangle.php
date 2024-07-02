9762ad726a2ef73eaa7b06cdefaa6178989247c5
vahid <vahidabdi.me@gmail.com>
2024-07-02 15:15:13 +0330

    Add Triangle domain model with unit tests

---
+++ src/Domain/Model/Triangle.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Domain\Model;
+
+
+use App\Domain\Contract\Shape2D;
+
+readonly class Triangle implements Shape2D
+{
+    private const TYPE = 'triangle';
+
+    public function __construct(private int $a, private int $b, private int $c)
+    {
+        if ($a <= 0 || $b <= 0 || $c <= 0) {
+            throw new \InvalidArgumentException('All side values must be positive numbers');
+        }
+        if (($a + $b <= $c) || ($a + $c <= $b) || ($b + $c <= $a)) {
+            throw new \InvalidArgumentException('Invalid triangle');
+        }
+    }
+
+    public function calculateSurface(): float
+    {
+        // Using Heron's formula
+        $s = $this->calculateCircumference() / 2;
+
+        return sqrt($s * ($s - $this->a) * ($s - $this->b) * ($s - $this->c));
+    }
+
+    public function calculateCircumference(): float
+    {
+        return $this->a + $this->b + $this->c;
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+}
---
+++ tests/Unit/TriangleTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Domain\Model\Triangle;
+use PHPUnit\Framework\TestCase;
+
+class TriangleTest extends TestCase
+{
+    public function testCircumferenceCanBeCalculated(): void
+    {
+        $triangle = new Triangle(3, 4, 5);
+
+        $circumference = $triangle->calculateCircumference();
+
+        $this->assertSame(12.00, (float) number_format($circumference, 2));
+    }
+
+    public function testSurfaceCanBeCalculated(): void
+    {
+        $triangle = new Triangle(3, 4, 5);
+
+        $surface = $triangle->calculateSurface();
+
+        $this->assertSame(6.00, (float) number_format($surface, 2));
+    }
+
+    /**
+     * @dataProvider nonPositiveSideTriangleDataProvider
+     */
+    public function testAllSidesArePositiveNumbers(float $a, float $b, float $c): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new Triangle($a, $b, $c);
+    }
+
+    /**
+     * @dataProvider validTriangleDataProvider
+     */
+    public function testIsValidTriangle(float $a, float $b, float $c, bool $isValid)
+    {
+        if ($isValid === false) {
+            $this->expectException(\InvalidArgumentException::class);
+        } else {
+            $this->assertTrue(true);
+        }
+        new Triangle($a, $b, $c);
+    }
+
+    public function nonPositiveSideTriangleDataProvider(): array
+    {
+        return [
+            [0, 4, 5],
+            [3, 0, 5],
+            [3, 4, 0],
+        ];
+    }
+
+    public function validTriangleDataProvider(): array
+    {
+        return [
+            // Valid triangles
+            [3, 4, 5, true],
+            [5, 5, 5, true],
+            [7, 10, 5, true],
+
+            // Invalid triangles
+            [1, 2, 3, false],
+            [5, 1, 1, false],
+            [0, 0, 0, false],
+            [-1, -2, -3, false],
+
+            // Degenerate triangles
+            [1, 1, 2, false],
+            [2, 3, 5, false]
+        ];
+    }
+}
